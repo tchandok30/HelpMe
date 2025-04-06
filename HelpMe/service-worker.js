220a11edcd8c18bf93b7513f99e6cd5cf6bfdfc8
tchandok30 <tanishachandok@gmail.com>
2025-04-06 09:20:52 +0530

    Update service-worker.js

--- HelpMe/service-worker.js
+++ HelpMe/service-worker.js
@@ -1,23 +1,26 @@
 // Simple cache for offline
-self.addEventListener('install', e => {
-    e.waitUntil(
-      caches.open('helpme-cache').then(cache => {
-        return cache.addAll([
-          '/',
-          '/index.html',
-          '/style.css',
-          '/app.js',
-          '/firebase.js'
-        ]);
-      })
-    );
-  });
+const CACHE_NAME = 'helpme-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/14658292_2011.i211.029_loudspeaker megaphone lightnings realistic (3).jpg',
+  '/3325437_458738-PFWZHQ-65.jpg'
   
-  self.addEventListener('fetch', e => {
-    e.respondWith(
-      caches.match(e.request).then(response => {
-        return response || fetch(e.request);
-      })
-    );
-  });
-  
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
